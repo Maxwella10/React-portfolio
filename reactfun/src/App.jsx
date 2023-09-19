07ed3eadf781f87aefbd5bf1e0945eb183844f51
Maxwella10 <96734464+Maxwella10@users.noreply.github.com>
2023-09-19 19:44:49 +0100

    setting up react-router-dom

--- reactfun/src/App.jsx
+++ reactfun/src/App.jsx
@@ -1,16 +1,23 @@
-import './App.css'
-import Header from "./Components/Header"
+import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from './components/Header';
+import Home from "./pages/Home"
+import ProjectGallery from "./pages/ProjectGallery"
+import Contact from "./pages/Contact"
 
 function App() {
-
   return (
     <>
-   
       <Header />
-      
-     
-    </> 
-  )
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/projectGallery" component={ProjectGallery} />
+          <Route exact path="/contact" component={Contact} />
+        </Switch>
+      </Router>
+    </>
+  );
 }
 
-export default App
+export default App;
